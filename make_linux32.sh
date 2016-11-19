759d2ad2eb1653e25db236ce5f406bfbc155e10b
orkim <d.orkim@gmail.com>
2016-11-19 14:42:53 +0000

    added missed libraries to build script

--- make_linux32.sh
+++ make_linux32.sh
@@ -5,4 +5,4 @@ gcc -c -static base64.c
 gcc -c -static DH1080.c
 gcc -fPIC -shared DH1080.o base64.o -o DH1080.so
 gcc -static -I${TCLINC} -c DH1080_tcl.c
-gcc -static -shared DH1080_tcl.o DH1080.o base64.o -o DH1080_tcl.so
+gcc -static -shared DH1080_tcl.o DH1080.o base64.o -lssl -lcrypto -o DH1080_tcl.so
--- make_linux64.sh
+++ make_linux64.sh
@@ -5,4 +5,4 @@ gcc -fPIC -c -static base64.c
 gcc -fPIC -c -static DH1080.c
 gcc -fPIC -shared DH1080.o base64.o -o DH1080.so
 gcc -fPIC -I${TCLINC} -c DH1080_tcl.c
-gcc -fPIC -shared DH1080_tcl.o DH1080.o base64.o -o DH1080_tcl.so
+gcc -fPIC -shared DH1080_tcl.o DH1080.o base64.o -lssl -lcrypto -o DH1080_tcl.so
